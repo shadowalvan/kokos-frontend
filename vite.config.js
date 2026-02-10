0027f5f1fd9861246dfe09964bcaf38a194843a8
alvan <152489710+shadowalvan@users.noreply.github.com>
2026-02-11 01:57:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,3 +9,12 @@ export default defineConfig(({ mode }) => {
   plugins: [react()],    
   }
 })
+*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+})
+
